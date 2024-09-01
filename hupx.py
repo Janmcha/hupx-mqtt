0dac508586ace150b8a90c704c6442e9f1661a3d
Janmcha <160396823+Janmcha@users.noreply.github.com>
2024-09-01 16:38:25 +0200

    just trying to get everything working

--- hupx.py
+++ hupx.py
@@ -4,7 +4,9 @@ import json
 import pandas as pd
 import paho.mqtt.client as mqtt
 import datetime
-debug = True
+from datetime import date, timedelta
+mqtt_dict = []
+debug = False
 
 def avg_extremes_index_of_three(numbers):
 
@@ -40,65 +42,51 @@ def avg_extremes_index_of_three(numbers):
 	if(debug):
 		print(min_average, min_index, max_average, max_index)
 
-	return min_average, min_index, max_average, max_index
+	return min_index, min_average, max_index, max_average
 
 
 
-def get_hupx_data():
-
-	# Step 1: Fetch the webpage content
-	response = requests.get("https://hupx.hu")
-
-	# Step 2: Parse the HTML content
-	soup = BeautifulSoup(response.content, "html.parser")
+def get_hupx_data(requested_date):
 
 
-	# Step 3: Find the <script> tag with id="dam-price-graph-data"
-	script_tag = soup.find("script", id="dam-price-graph-data")
+	# Define the URL
+	#this might change in the future
+	url = 'https://hupx.hu/en/dam/homepage/graph.json?date=' + str(requested_date)
 
+	# Define the headers
+	headers = {
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0',
+	'Accept': '*/*',
+	'Accept-Language': 'en-US,en;q=0.5',
+	'Accept-Encoding': 'gzip, deflate, br, zstd',
+	'Referer': 'https://hupx.hu/en/',
+	'Connection': 'keep-alive',
+	'Sec-Fetch-Dest': 'empty',
+	'Sec-Fetch-Mode': 'no-cors',
+	'Sec-Fetch-Site': 'same-origin',
+	'X-Requested-With': 'XMLHttpRequest',
+	'Priority': 'u=0',
+	'Pragma': 'no-cache',
+	'Cache-Control': 'no-cache'
+}
 
-	# Step 4: Extract the content from the script tag and load it as JSON
-	if script_tag:
-		# Assuming the script contains just the JSON data without any extra code
-		json_data = json.loads(script_tag.string)
+	# Make the GET request
+	response = requests.get(url, headers=headers)
 
-		# If there is additional text around the JSON, you'll need to extract the JSON part
-		# For example, if the script contains something like "var data = {...};"
-		# json_str = script_tag.string.strip().split('=')[-1].strip(' ;')
-		# json_data = json.loads(json_str)
+	# Check if the request was successful
+	if response.status_code == 200:
+		# Print or process the JSON data
+		json_data = response.json()
 		if(debug):
 			print(json_data)
 	else:
-		#they might change the tag in the future 
-		print("Script tag with id 'dam-price-graph-data' not found.")
-
-	#getting the date of the latest available data, if it is in the future, then the data for the next day is available by now
-	latest_data_date = soup.find("div", class_="flatpickr-wrap")
-	if latest_data_date:
-		max_date = latest_data_date.get("data-max-date")
-		if(debug):
-			print(f"Max Date: {max_date}")
-	else:
-		##they might also change the tag in the future
-		print("Div tag with class 'flatpickr-wrap' not found.")
-
-
-	if (max_date>datetime.datetime.today().strftime('%Y-%m-%d')):
-		next_days_data_available = True
-	else:
-		next_days_data_available = False
+		print(f"Failed to retrieve data: {response.status_code}")
 
 	columns = [col['label'] for col in json_data['cols']]
 
 	# Create DataFrame
 	df = pd.DataFrame(json_data['rows'], columns=columns)
 
-	if(debug):
-	# Display the DataFrame and the latest available data's date
-		print(max_date)
-		print(df)
-		print(next_days_data_available)
-	#creating a zero indexed list from the prices 
 	prices = df.iloc[:, 2].values.tolist()
 	if(debug):
 		print(prices)
@@ -119,24 +107,169 @@ def send_data_via_mqtt(ip,username,password,data):
 	client.disconnect()
 
 
-def create_mqtt_dict():
-#creating a dictionary from the data to send in send_data_via_mqtt
-	mqtt_dict =[
-		{"topic": "/home/hupx/todaysmineuro", "value": 30},
-		{"topic": "/home/hupx/todaysminhour", "value": 30},
-		{"topic": "/home/hupx/todaysmaxeuro", "value": 30},
-		{"topic": "/home/hupx/todaysmaxhour", "value": 30},
-		{"topic": "/home/hupx/todaysmin3hblockstart", "value": 30},
-		{"topic": "/home/hupx/todaysmax3hblockstart", "value": 30},
-		{"topic": "/home/hupx/tomorrowsmineuro", "value": 30},
-		{"topic": "/home/hupx/tomorrowsminhour", "value": 30},
-		{"topic": "/home/hupx/tomorrowsmaxeuro", "value": 30},
-		{"topic": "/home/hupx/tomorrowssmaxhour", "value": 30},
-		{"topic": "/home/hupx/tomorrowssmin3hblockstart", "value": 30},
-		{"topic": "/home/hupx/tomorrowsmax3hblockstart", "value": 30},
-		]
+
+
+def calculate_required_data(todays_price_list,tomorrows_price_list):
+
+
+	todays_minimum_price = min(todays_price_list)
+	todays_maximum_price = max(todays_price_list)
+	todays_minimum_price_hour = todays_price_list.index(min(todays_price_list))
+	todays_maximum_price_hour = todays_price_list.index(max(todays_price_list))
+	tomorrows_minimum_price = min(tomorrows_price_list)
+	tomorrows_maximum_price = max(tomorrows_price_list)
+	tomorrows_minimum_price_hour = tomorrows_price_list.index(min(tomorrows_price_list))
+	tomorrows_maximum_price_hour = tomorrows_price_list.index(max(tomorrows_price_list))
+
+	#get 3 hour block data
+	todaysmin3hblockstart, todaysmin3hblockaverage, todaysmax3hblockstart, todaysmax3hblockaverage = avg_extremes_index_of_three(todays_price_list)
+	tomorrowsmin3hblockstart, tomorrowsmin3hblockaverage, tomorrowsmax3hblockstart, tomorrowsmax3hblockaverage = avg_extremes_index_of_three(tomorrows_price_list)
+
+	#get the historical data
+	historical_data = calculate_historical_data()
+
+
+	#if we have the exact same data for both request that means the data is not available yet and the server responded with the latest available data which is todays
+	if(todays_price_list!=tomorrows_price_list):
+		tomorrows_data_available=True
+
+
+		#creating the mqtt dictionary
+		global mqtt_dict
+		mqtt_dict =[
+				#todays data
+				{"topic": "/home/hupx/todaysmineuro", "value": todays_minimum_price},
+				{"topic": "/home/hupx/todaysminhour", "value": todays_minimum_price_hour},
+				{"topic": "/home/hupx/todaysmaxeuro", "value": todays_maximum_price},
+				{"topic": "/home/hupx/todaysmaxhour", "value": todays_maximum_price_hour},
+				{"topic": "/home/hupx/todaysmin3hblockstart", "value": todaysmin3hblockstart},
+				{"topic": "/home/hupx/todaysmin3hblockaverage", "value": todaysmin3hblockaverage},
+				{"topic": "/home/hupx/todaysmax3hblockstart", "value": todaysmax3hblockstart},
+				{"topic": "/home/hupx/todaysmax3hblockaverage", "value": todaysmax3hblockaverage},
+
+				#tomorrows data
+				{"topic": "/home/hupx/tomorrowsmineuro", "value": tomorrows_minimum_price},
+				{"topic": "/home/hupx/tomorrowsminhour", "value": tomorrows_minimum_price_hour},
+				{"topic": "/home/hupx/tomorrowsmaxeuro", "value": tomorrows_maximum_price},
+				{"topic": "/home/hupx/tomorrowssmaxhour", "value": tomorrows_maximum_price_hour},
+				{"topic": "/home/hupx/tomorrowsmin3hblockstart", "value": tomorrowsmin3hblockstart},
+				{"topic": "/home/hupx/tomorrowsmin3hblockaverage", "value": tomorrowsmin3hblockaverage},
+				{"topic": "/home/hupx/tomorrowsmax3hblockstart", "value": tomorrowsmax3hblockstart},
+				{"topic": "/home/hupx/tomorrowsmax3hblockaverage", "value": tomorrowsmax3hblockaverage},
+
+				#historical data
+				{"topic": "/home/hupx/7daysminprice", "value": historical_data[0]},
+				{"topic": "/home/hupx/7daysmindate", "value": historical_data[1]},
+				{"topic": "/home/hupx/30daysminprice", "value": historical_data[2]},
+				{"topic": "/home/hupx/30daysmindate", "value": historical_data[3]},
+				{"topic": "/home/hupx/90daysminprice", "value": historical_data[4]},
+				{"topic": "/home/hupx/90daysmindate", "value": historical_data[5]},
+
+				{"topic": "/home/hupx/7daysmaxprice", "value": historical_data[6]},
+				{"topic": "/home/hupx/7daysmaxdate", "value": historical_data[7]},
+				{"topic": "/home/hupx/30daysmaxprice", "value": historical_data[8]},
+				{"topic": "/home/hupx/30daysmaxdate", "value": historical_data[9]},
+				{"topic": "/home/hupx/90daysmaxprice", "value": historical_data[10]},
+				{"topic": "/home/hupx/90daysmaxdate", "value": historical_data[11]},
+
+
+
+			]
+
+		if(debug):
+			print(mqtt_dict)
+
+	else:
+		tomorrows_data_available=False
+		#creating the mqtt dictionary leaving out tomorrows data, so it will go stale in the mqtt broker triggering it to be unavailable
+		global mqtt_dict
+		mqtt_dict =[
+				#todays data
+				{"topic": "/home/hupx/todaysmineuro", "value": todays_minimum_price},
+				{"topic": "/home/hupx/todaysminhour", "value": todays_minimum_price_hour},
+				{"topic": "/home/hupx/todaysmaxeuro", "value": todays_maximum_price},
+				{"topic": "/home/hupx/todaysmaxhour", "value": todays_maximum_price_hour},
+				{"topic": "/home/hupx/todaysmin3hblockstart", "value": todaysmin3hblockstart},
+				{"topic": "/home/hupx/todaysmin3hblockaverage", "value": todaysmin3hblockaverage},
+				{"topic": "/home/hupx/todaysmax3hblockstart", "value": todaysmax3hblockstart},
+				{"topic": "/home/hupx/todaysmax3hblockaverage", "value": todaysmax3hblockaverage},
+
+				#historical data
+				{"topic": "/home/hupx/7daysminprice", "value": historical_data[0]},
+				{"topic": "/home/hupx/7daysmindate", "value": historical_data[1]},
+				{"topic": "/home/hupx/30daysminprice", "value": historical_data[2]},
+				{"topic": "/home/hupx/30daysmindate", "value": historical_data[3]},
+				{"topic": "/home/hupx/90daysminprice", "value": historical_data[4]},
+				{"topic": "/home/hupx/90daysmindate", "value": historical_data[5]},
+
+				{"topic": "/home/hupx/7daysmaxprice", "value": historical_data[6]},
+				{"topic": "/home/hupx/7daysmaxdate", "value": historical_data[7]},
+				{"topic": "/home/hupx/30daysmaxprice", "value": historical_data[8]},
+				{"topic": "/home/hupx/30daysmaxdate", "value": historical_data[9]},
+				{"topic": "/home/hupx/90daysmaxprice", "value": historical_data[10]},
+				{"topic": "/home/hupx/90daysmaxdate", "value": historical_data[11]},
+
+
+
+			]
 
 
 
 
-print(get_hupx_data())
+
+
+def calculate_historical_data():
+	weekly_min_price = float('inf')
+	monthly_min_price = float('inf')
+	quarterly_min_price = float('inf')
+
+	weekly_max_price = float('-inf')
+	monthly_max_price = float('-inf')
+	quarterly_max_price = float('-inf')
+
+	for i in range(1, 90):
+		prices = get_hupx_data(date.today()-timedelta(days=i))
+		lowest_price = min(prices)
+		highest_price = max(prices)
+
+		if(lowest_price<=weekly_min_price) and i<8:
+			weekly_min_price = lowest_price
+			weekly_min_price_date = str(date.today()-timedelta(days=i))
+
+		if(lowest_price<=monthly_min_price) and i<30:
+			monthly_min_price = lowest_price
+			monthly_min_price_date = str(date.today()-timedelta(days=i))
+
+		if(lowest_price<=quarterly_min_price) and i<90:
+			quarterly_min_price = lowest_price
+			quarterly_min_price_date = str(date.today()-timedelta(days=i))
+
+		if(highest_price>=weekly_max_price) and i<8:
+			weekly_max_price = highest_price
+			weekly_max_price_date = str(date.today()-timedelta(days=i))
+
+		if(highest_price>=monthly_max_price) and i<30:
+			monthly_max_price = highest_price
+			monthly_max_price_date = str(date.today()-timedelta(days=i))
+
+		if(highest_price>=quarterly_max_price) and i<90:
+			quarterly_max_price = highest_price
+			quarterly_max_price_date = str(date.today()-timedelta(days=i))
+
+
+	return (weekly_min_price,
+		weekly_min_price_date,
+		monthly_min_price,
+		monthly_min_price_date,
+		quarterly_min_price,
+		quarterly_min_price_date,
+		weekly_max_price,
+		weekly_max_price_date,
+		monthly_max_price,
+		monthly_max_price_date,
+		quarterly_max_price,
+		quarterly_max_price_date
+		)
+
+calculate_required_data(get_hupx_data(date.today()),get_hupx_data(date.today()+timedelta(days=1)))
+print(calculate_historical_data())
